fc872faf06227febc8c548bad5b7c62941b0fb95
Stijn De Ridder <deridder.stijn93@gmail.com>
2018-07-31 19:48:32 +0200

    basic function for spawning minions

--- Classes/Tower.php
+++ Classes/Tower.php
@@ -1,5 +1,7 @@
 <?php 
+
 namespace Classes;
+
 class Tower{
     private $name = "";
     private $gold = 0;
@@ -12,16 +14,22 @@ class Tower{
         $this->health = $health;
     }
 
-    public function spawnMinion(){
-
+    public function spawnMinion($name, $type, $range, $health, $damage, $speed){
+        $location = ($this->leftRight === "left") ? 0 : 20;
+        //todo: check if the tower has enough money to pay for the minion
+        if($this->gold >= 50){
+            setGold(-50);
+            $this->minions = new Minion($name, $type, $range, $health, $damage, $speed, $orientation);
+        }
     }
     
-    public function takeDamage():int {
-
+    public function takeDamage($damage):int {
+        $this->health -= $damage;
+        return $this->health;
     }
 
     public function setGold(int $amount):int{
-
+        $this->gold += $amount;
     }
 
     public function getName():string{
